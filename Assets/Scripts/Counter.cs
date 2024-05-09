3785ffbdde38c9d2aab6cb08e71ab7e393fb2233
WhiteFang4221 <artemka7331@bk.ru>
2024-05-09 16:24:22 +0300

    Update Counter.cs
    Исправил вечную работу корутины и вывел изменение ее работы в отдельный метод

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -7,31 +7,41 @@ public class Counter : MonoBehaviour
     [SerializeField] private TMP_Text _counter;
 
     private int _currentValue = 0;
-    private WaitForSeconds _delay = new WaitForSeconds (0.5f);
+    private WaitForSeconds _delay = new WaitForSeconds(0.5f);
     private bool _isCounting = false;
 
     private void Start()
     {
         _counter.text = ($"—четчик: {_currentValue}");
-        StartCoroutine(nameof(ChangeCounter));
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _isCounting = !_isCounting;
+            ChangeCounterState();
         }
     }
 
-    private IEnumerator ChangeCounter()
+    private void ChangeCounterState()
+    {
+        if (_isCounting == false)
+        {
+            _isCounting = true;
+            StartCoroutine(nameof(IncreaseCounter));
+        }
+        else if (_isCounting == true)
+        {
+            _isCounting = false;
+            StopCoroutine(nameof(IncreaseCounter));
+        }
+    }
+
+    private IEnumerator IncreaseCounter()
     {
         while (true)
         {
-            if (_isCounting)
-            {
-                _counter.text = ($"—четчик: {++_currentValue}");
-            }
+            _counter.text = ($"—четчик: {++_currentValue}");
 
             yield return _delay;
         }
